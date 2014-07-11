a6538554932b2e050e35d4985825734c84d8f2b9
Your Name <dorjanjo@gmail.com>
2014-07-11 01:02:56 +0000

    third commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Doreen's Portal</title>
+  <link rel="stylesheet" href="css/styles.css" type="text/css"/>
+</head>
+
+<body>
+
+<div id="header">
+  <h1>Welcome to Doreen's Portal Page!</h1>
+  <h2>Web 130 - Web Authoring</h2>
+</div>
+  
+<?php include 'includes/nav.php' ?>
+
+<div id="section">
+  <section>
+  <h2>Web Authoring III</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc convallis, justo a bibendum luctus, erat enim tempor eros, vitae dapibus metus enim sed quam. Donec at nibh massa. Fusce sed aliquet sapien. Suspendisse sit amet urna lectus. Donec ut augue ante. Fusce tempus diam non risus iaculis, non aliquet dui vestibulum. Suspendisse laoreet purus vulputate porta pellentesque. Aenean erat orci, feugiat in lacus eget, aliquam fringilla turpis. Fusce ac arcu id odio sodales mattis eu at neque. Vivamus ullamcorper dolor a lacus rutrum, id interdum diam eleifend. Nunc imperdiet sem eget metus vestibulum, vel rutrum ligula condimentum. Aenean facilisis interdum tristique.</p>
+  <section> 
+</div>
+
+<div id="aside">
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+</div>
+
+<div id="footer">
+  <p>&copy; Doreen's Portal 2014</p>
+</div>
+
+</body>
+
+</html>
